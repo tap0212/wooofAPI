8c320e3f50222c1eba9e74b793f39832459e27a4
tap0212 <40211415+tap0212@users.noreply.github.com>
2020-04-29 23:19:14 +0530

    save

--- controllers/product.js
+++ controllers/product.js
@@ -143,13 +143,13 @@ exports.removeProduct = (req,res) => {
 
 
 exports.getAllProducts = (req,res) => {
-    let limit = req.query.limit ? parseInt(req.query.limit) : 8;
+   
     let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
     Product.find()
         .select("-photo")
         .populate("category")
         .sort([[sortBy, "asc"]])
-        .limit(limit)
+  
         .exec((err,products) => {
             if(err){
                 return res.status(400).son({
